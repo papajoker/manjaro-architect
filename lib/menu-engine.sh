f8bd65fc37279fcee76a2ff9644f9ad673dfe76b
papajoker <patrickc538@gmail.com>
2017-03-16 02:15:33 +0100

    simplify strcture

--- lib/menu-engine.sh
+++ lib/menu-engine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# load layout ans make dialogs
+# load layout and show dialogs
 
 
 dialog_menu() {
@@ -27,15 +27,6 @@ tt() {
         fi
     fi
     echo "$str"
-}
-
-# reset numbers after delete/insert item
-renumber_menutool() {
-    local i=0 j=0
-    for i in "${!menus[@]}"; do
-        ((i % 2==0)) && { ((j=j+1)); menus[$i]=$j; }
-        ((i=i+1))
-    done
 }
 
 # trim line with ; as separator
@@ -70,8 +61,8 @@ menu_split_line(){
 menu_item_find() {
     [ -z "$1" ] && return 1
     local id=0 i=0
-    for i in "${!menus[@]}"; do
-        if [[ "${menus[$i]}" == "$1" ]]; then
+    for i in "${!options[@]}"; do
+        if [[ "${options[$i]}" == "$1" ]]; then
             (( id= (i-1) /2 ))
             echo "$id" && return 0
         fi
@@ -92,12 +83,12 @@ menu_item_change() {
         unset functions[$id]
         functions=( "${functions[@]}" )
         (( id=(id*2) ))
-        unset menus[$((id+1))]; unset menus[$id];
-        menus=( "${menus[@]}" )
+        unset options[$((id+1))]; unset options[$id];
+        options=( "${options[@]}" )
     else
         [ -n "$3" ] && functions[$id]="$3"
         (( id=(id*2)+1 ))
-        [ -n "$2" ] && menus[$id]="$2"
+        [ -n "$2" ] && options[$id]="$2"
     fi
 }
 
@@ -111,21 +102,7 @@ menu_item_insert () {
     (( id=id+1 ))
     functions=( "${functions[@]:0:id}" "$3" "${functions[@]:id}" )
     (( id=id*2 ))
-    menus=( "${menus[@]:0:id}" "9" "$2" "${menus[@]:id}" )
-}
-
-# returned fy function load_options_menutool()
-declare -A mnu_params=(
-    [functions]=''
-    [begin]=''
-    [end]=''
-)
-
-menu_reset_mnu_params(){
-    declare -g mnu_params
-    mnu_params[functions]=''
-    unset mnu_params[begin]
-    unset mnu_params[end]
+    options=( "${options[@]:0:id}" "9" "$2" "${options[@]:id}" )
 }
 
 ####################################################
@@ -136,12 +113,13 @@ menu_reset_mnu_params(){
 
 load_options_menutool() {
     local menukey="${1:-install_environment}"
-    local menuglobal_items=() menuglobal_fn=() tmp=()
+    local tmp=()
     local localfilename="${2:-default}"
     local level='' levelsub='' reg loop curentitem i j
     local IFS=$'\n'
     debug "\nfind menu: $menukey"    
-    menu_reset_mnu_params
+    options=(); functions=(); fbegin=''; fend='';floop='';
+
     for line  in $(grep -v "#" "$DATADIR/${localfilename}.menu"); do
         #reg="-.$menukey^[a-zA-Z_;]" #[^a-zA-Z_]"
         reg="-.${menukey}([ ;]|$)"
@@ -152,8 +130,9 @@ load_options_menutool() {
             # functions check before and after loop
             line=$(menu_split_line "$line")
             IFS=';' line=($line)
-            (("${#line[@]}">1)) && mnu_params[begin]="${line[1]}"
-            (("${#line[@]}">2)) && mnu_params[end]="${line[2]}"
+            (("${#line[@]}">1)) && fbegin="${line[1]}"
+            (("${#line[@]}">2)) && fend="${line[2]}"
+            (("${#line[@]}">3)) && floop="${line[3]}"
             debug "key:$menukey find :) ${mnu_params[@]}"
             continue
         fi
@@ -163,41 +142,29 @@ load_options_menutool() {
                 # menu item find
                 line=$(menu_split_line "$line")
                 IFS=';' line=($line)
-                #debug "ok, line parsed: $line"
                 IFS=$';\n' line=(${line[*]})
                 curentitem="${line[0]}"
                 
                 debug "curitem: $curentitem , function: ${line[1]} "
-                menuglobal_items+=( "$(tt $curentitem Title)" )
+                options+=( 0 "$(tt $curentitem Title)" )
                 # attach function to item
                 tmp="${line[1]}"
                 tmp="${tmp:-returnOK}" # for this test
-                menuglobal_fn+=( "${tmp}" )
+                functions+=( "${tmp}" )
             else
                 reg="^$levelsub--[a-zA-Z_]"
                 if [[ $line =~ $reg ]]; then
                     # prev-item load a sub menu
-                    menuglobal_fn[-1]="show_menu $curentitem"
-                    i=$(( "${#menuglobal_items[@]}"-1 ))
-                    tmp="${menuglobal_items[-1]}"
-                    [[ "${tmp: -1}" != ">" ]] && menuglobal_items[-1]="${tmp} |>"
+                    functions[-1]="show_menu $curentitem"
+                    i=$(( "${#options[@]}"-1 ))
+                    tmp="${options[-1]}"
+                    [[ "${tmp: -1}" != ">" ]] && options[-1]="${tmp} |>"
                     continue
                 fi
                 reg="^$level[a-zA-Z_]"
                 [[ $line =~ $reg ]] && break
             fi
         fi
-    done
-
-    # save function for each items
-    for i in "${menuglobal_fn[@]}"; do
-        mnu_params[functions]="${mnu_params[functions]}${i};"
-    done
-    for i in "${!menuglobal_items[@]}"; do
-        (( j=i+1 ))
-        debug "$j  \"${menuglobal_items[$i]}\" \"${menuglobal_fn[$i]}\" "
-        echo "$j"
-        echo "${menuglobal_items[$i]}"
     done
 }
 
@@ -208,28 +175,32 @@ load_options_menutool() {
 
 show_menu()
 {
-    local cmd id menus choice keymenu errcode highlight=1 nbitems=1
-    local fend functions
-#    declare functions
-    keymenu="${1:-MM}"
+    local cmd id menus choice errcode highlight=1 nbitems=1
+    local fend fbegin floop            # check functions
+    local options=() functions=()   # menu datas
+    local keymenu="${1:-MM}"
 
-    load_options_menutool "${keymenu}">'/tmp/mnu'
-    # a function to remove  the third column
-    mapfile -t menus < '/tmp/mnu'
-    declare -p menus &>/dev/null
-    IFS=';' read -a functions <<<"${mnu_params[functions]}"
+    # reset numbers after delete/insert item
+    renumber_menutool() {
+        local i=0 j=0
+        for i in "${!options[@]}"; do
+            ((i % 2==0)) && { ((j=j+1)); options[$i]=$j; }
+            ((i=i+1))
+        done
+    }
+
+    load_options_menutool "${keymenu}"
+    renumber_menutool
+    debug "options: ${options[@]}"
     debug "functions: ${functions[@]}"
 
     # call function chech  begin
-    if [ -n "${mnu_params[begin]}" ]; then
-        ${mnu_params[begin]} $keymenu || return 99
+    if [ -n "${fbegin}" ]; then
+        $fbegin "$keymenu" || return 99
         renumber_menutool # if insert or delete item
     fi
-    if [ -n "${mnu_params[end]}" ]; then
-        fend="${mnu_params[end]}"
-    fi
 
-    (( nbitems="${#menus[@]}" /2 ))
+    (( nbitems="${#options[@]}" /2 ))
     ((nbitems>20)) && nbitems=20 # show max 20 items
 
     while ((1)); do
@@ -237,16 +208,16 @@ show_menu()
         cmd+=( $nbitems ) # add number of items
 
         # run dialog options
-        choice=$("${cmd[@]}" "${menus[@]}" 2>&1 >/dev/tty)
-        # ?choice=eval "${cmd[@]}" "${menus[@]}" 2>&1 >/dev/tty
+        choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        # ?choice=eval "${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty
         choice="${choice:-0}"
 
         case "$choice" in
             0)  return ;;                # btn cancel
             *)  debug "\nchoice: $choice"               # debug
                 [ -z "$choice" ] && continue
-                id=$(( "$choice"-1 ))
-                fn="${functions[$id]}"      # find attach working function to array with  3 column
+                ((--choice))
+                fn="${functions[$choice]}"      # find attach working function to array with  3 column
                 debug "call function: $fn"
                 # or use eval for eval ${fn} if variables $var ? nor secure ?
                 ${fn}
@@ -254,22 +225,34 @@ show_menu()
                 debug "errcode: $errcode"
                 case "$errcode" in
                     99) :   # check_before in sub_menu
+                        ;;                
+                    98) :   # check_end in sub_menu
                         ;;
-                    98) :    # check_end in sub menu
+                    97) :    # back - prev menu
+                        return 0
                         ;;
                     0) :
                         ((++choice))        # go to next item if ok
                         highlight=$choice
                         ;;
                     *) :    # other errors
-                        break
+                        #(( ($choice+1)*2 ))
+                        DIALOG " ERROR " --msgbox "\n ${options[(( (--$choice*2)+1 ))]} [errcode:${errcode}] \n " 0 0
+                        # return 0 not return
                         ;;
                 esac
         esac
 
+        # call function chech  loop
+        if [ -n "${floop}" ]; then
+            $floop "$keymenu" || return 98
+            renumber_menutool # if insert or delete item
+        fi
+
     done
+
     # call function check end in layout
     if [ -n "${fend}" ]; then
-        ${fend} $keymenu || return 98
+        $fend "$keymenu" || return 98
     fi
 }
